fd8234793aab6bf5b9586028791183a15f12a638
OskarLindblad <oskar@eperoto.com>
2021-04-29 09:07:41 +0200

    Add eperotostudio

--- src/client.js
+++ src/client.js
@@ -3,4 +3,7 @@ import sanityClient from "@sanity/client";
 export default sanityClient({
   projectId: "1ta3690e",
   dataset: "production",
+  apiVersion: "2021-03-25", // use a UTC date string
+  token: "", // or leave blank for unauthenticated usage
+  useCdn: true, // `false` if you want to ensure fresh data
 });
