0e9e4b808b716795fb0c9942163d97d98842383c
OskarLindblad <oskar@eperoto.com>
2021-05-20 13:51:29 +0200

    some small margin...

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import eperotoLogo from "../images/Eperoto-Logo-1.png";
 import eperotoLogoDark from "../images/Eperoto-Logo-Dark.png";
